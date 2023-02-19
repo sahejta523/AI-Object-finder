1398657f12acd00327e91e7e073a0118c74a63dd
sahejta523 <88138423+sahejta523@users.noreply.github.com>
2023-02-19 05:36:06 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+status="";
+
+function setup(){
+    canvas = createCanvas(380,380);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.size(380,380);
+video.hide();
+}
+function start(){
+    objectDetector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+object_name=document.getElementById("object_name").value;
+}
+function modelLoaded(){
+    console.log("Model Loaded!");
+    status = true;
+}
+function draw(){
+    image(video,0,0,380,380);
+}
